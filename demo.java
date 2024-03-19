43e76ae7ef27e0b873d2c909dab964bdd1d9b6a9
Ravi <ravisingh.rgh.98@gmail.com>
2024-03-19 16:21:58 +0530

    Modified JAVA & Text files

--- demo.java
+++ demo.java
@@ -1,4 +1,6 @@
 public static void main(String [] args)
 {
 system.out.println("hello world")
+system.out.println("welcome to basics of DevOps Session")
+
 }
--- readme.txt
+++ readme.txt
@@ -1,2 +1,3 @@
 hii 
 welcome to basics devops session
+welcome to the session of 19th march 2024.
